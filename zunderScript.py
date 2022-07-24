4fee282a60eab6011ca89527fe20b00f56006baf
Rterfvggggf <91810880+Rterfvggggf@users.noreply.github.com>
2022-07-24 12:44:28 +0000

    Read/Input

--- zunderScript.py
+++ zunderScript.py
@@ -1,7 +1,17 @@
+from distutils.ccompiler import show_compilers
+
 def zund_print():
-    
+
     zund=print("")
     if zund==print(""):
         print(zund)
 
 zund_print()
+
+def zund_input():
+    read=0
+    zund=read
+    if zund==read:
+        i=input("Bitte eingabe tätigen : ")
+        print(i)
+zund_input()
