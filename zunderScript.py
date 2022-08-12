04a73302c9c9560ca33a19d3bc3203b91797d1b5
SembiaStudios <91810880+SembiaStudios@users.noreply.github.com>
2022-08-12 21:30:52 +0200

    Update zunderScript.py

--- zunderScript.py
+++ zunderScript.py
@@ -6,7 +6,7 @@ def zund_print():
     if zund==print(""):
         print(zund)
 
-zund_print()
+zund.io()
 
 def zund_input():
     read=0
@@ -14,4 +14,11 @@ def zund_input():
     if zund==read:
         i=input("Bitte eingabe tätigen : ")
         print(i)
-zund_input()
+zund.io()
+
+def zunder.help():
+    x = input()
+    if x == ("help"):
+        print("zund_print("") ist für einfache print befehle.")
+        print("zund.io() ist für inputs gedacht.")
+zunder.help()
